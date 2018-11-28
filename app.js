045872140541fd0c38bd98acb4b325608f158869
m0se <mhojmose@hotmail.com>
2018-11-28 17:08:56 +0100

    så

--- app.js
+++ app.js
@@ -21,16 +21,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
 
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
 app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  res.status(err.status || 500);
-  res.render('error');
 });
 
 app.all('/', function(request, response) {
