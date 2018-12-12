22e5cc5363399956426679c73d3e4bdba9052ed1
m0se <mhojmose@hotmail.com>
2018-12-12 17:17:21 +0100

    virk
    virk

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const baseUrl = "http://localhost:8010";
-//const baseUrl = "https://badjokes.herokuapp.com";
+//const baseUrl = "http://localhost:8010";
+const baseUrl = "https://badjokes.herokuapp.com";
 
 router.get('/', async function(req, res) {
 res.render('index')
