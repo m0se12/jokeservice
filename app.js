df981d91a56e4ef62aaa2703f25ecf3d735ebe81
m0se <mhojmose@hotmail.com>
2018-12-12 16:28:48 +0100

    Erik bliver glad

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
@@ -13,15 +12,10 @@ app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
 
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
-
 
 
 app.all('/', function(request, response) {
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,8 @@
 const express = require('express');
-const fetch = require('node-fetch');
 const router = express.Router();
 
-const api = require("../routes/api");
-
-//const baseUrl = "http://localhost:8010";
-const baseUrl = "https://badjokes.herokuapp.com";
-
+const baseUrl = "http://localhost:8010";
+//const baseUrl = "https://badjokes.herokuapp.com";
 
 router.get('/', async function(req, res) {
 res.render('index')
