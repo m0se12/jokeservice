19f3d5add0b41c2b5fea9f1136d6f6098a6efbfc
m0se <mhojmose@hotmail.com>
2018-12-12 14:40:51 +0100

    ny fejl

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ const fetch = require('node-fetch');
 const router = express.Router();
 
 
-//const baseUrl = "http://localhost:8010";
-const baseUrl = "https://badjokes.herokuapp.com";
+const baseUrl = "http://localhost:8010";
+//const baseUrl = "https://badjokes.herokuapp.com";
 
 
 router.get('/', async function(req, res, next) {
@@ -17,7 +17,7 @@ router.get('/', async function(req, res, next) {
 
     let jokes = await fetch(baseUrl+"/api/jokes").then(i=>i.json()).then(i=>{return i});
     let othersites = await fetch(baseUrl+"/api/othersites").then(i=>i.json()).then(i=>{return i});
-    let otherJokesResult = await fetch(baseUrl+"/api/allOtherJokes").then(i=>i.json()).then(i=>{return i});
+    let otherJokesResult = await fetch(baseUrl+"/api/allOtherJokes").then(x=>x.json()).then(i=>{return i});
 
     addToJsonArray("ownjokes",jokes);
     addToJsonArray("otherSources",othersites);
